21eea90b31b03a909a487c307be17777cfb8f8b2
John Higgins <johnhbenetech@users.noreply.github.com>
2016-02-11 14:33:54 -0800

    Update util.js

--- src/util.js
+++ src/util.js
@@ -223,7 +223,7 @@ define([
         var pos = 0;
         if (ctrl.createTextRange) {
             ctrl.focus ();
-            var sel = window.getSelection().getRangeAt(0);
+            var sel = window.getSelection();
             sel.moveStart ('character', -ctrl.value.length);
             pos = sel.text.length;
             
